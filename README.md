d4b84c2dc2a17bb31a7f881a4039396c3022fea6
SandraNAguayoN <Fifth1234>
2022-11-30 17:41:02 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Proyecto_ForAppNeo
+Propuesta de proyecto Integradora III.
+GDGS2102-E
+Aguayo Nuñez Sandra Nohelia
+Perez Vega Noemi
+Ramos Velazquez Carolina
+
---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const path = require('path');
+router.get('public', express.static(path.join(__dirname, 'public')));
+
+//Ir al la pantalla de inicio de la aplicación
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+//Pantalla sobre la app
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+module.exports = router;
